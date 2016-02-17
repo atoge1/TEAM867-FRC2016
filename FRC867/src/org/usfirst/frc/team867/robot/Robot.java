f2f5fff6ca82d40423975f48b9a03339d14555fc
atoge1 <atoge888@gmail.com>
2016-02-16 16:27:38 -0800

    added servo code

--- FRC867/src/org/usfirst/frc/team867/robot/Robot.java
+++ FRC867/src/org/usfirst/frc/team867/robot/Robot.java
@@ -4,7 +4,6 @@
 package org.usfirst.frc.team867.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -12,18 +11,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //additional imports
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Compressor;
 import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.DrawMode;
 import com.ni.vision.VisionException;
 import com.ni.vision.NIVision.Image;
-import com.ni.vision.NIVision.ShapeMode;
-
-
-
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Servo;
 
 
 /**
@@ -34,135 +28,156 @@ import edu.wpi.first.wpilibj.Servo;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
-    
-    //global variables
-    	
-    	//motors - note: make sure CAN devices are numbered properly in the web interface (connect roborio and go to 172.22.11.2). 
-    	//Start device IDs from 1. Leave 0 free for new devices.
-    	//code refers to device 1 with an index of 0
-    	CANTalon[] motorlist; //stores all motors
-    		
-    	//joysticks - note: buttons start at 1
-    	Joystick joyDrive; //driver joystick
-    	Joystick joyManip; //manipulator joystick
-    	
-    		//joystick values
-    		double leftDrive; //stores left joystick y axis value
-    		double rightDrive; //stores right joystick y axis value
-    		
-    				
-    	//compressor
-    	Compressor comp;
-    
-    	//valves
-    
-    	//camera
-    	int camSes0;
-    	int camSes1;
-    	Image frame;
-    	
-    	//controller axes
-        final int leftY = 1;
-        final int rightY = 5;
-        final int yButton = 4;
-        
-        
-        //state variables
-        boolean firstRun;
-        boolean useCam0;
-        boolean toggleCamera; //to check if in the process of toggle (true = can toggle, false = DO NOT TOGGLE YET)
-        
-    	
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
-    
-        
-        
-    //motorlist
-        motorlist = new CANTalon[6];
-        for(int i=0; i<=5; i++)
-        {
-        	motorlist[i] = new CANTalon(i+1);
-        }
-        
-        //enable brake mode for drive motors
-        for(int i=0; i<=3; i++)
-        {
-        	motorlist[i].enableBrakeMode(true);
-        }
-        
-    //joysticks
-        joyDrive = new Joystick(0);
-        joyManip = new Joystick(1);
-        
-        
-    //compressor
-        comp = new Compressor();
-        //enable
-        comp.start();
-        
-    //valves
-        
-    //camera
-        
-        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-        
-        //camSes0
-        try 
-        {
-        	camSes0 = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        }
-        catch(VisionException e)
-        {
-        	SmartDashboard.putString("Camera error @ camSes0 open", e.toString());
-        }
-        
-        try 
-        {
-        	NIVision.IMAQdxConfigureGrab(camSes0);
-        }
-        catch(VisionException e)
-        {
-        	SmartDashboard.putString("Camera error @ camSes0 configGrab", e.toString());
-        }
-        
-        
-        
-        //camSes1
-        try 
-        {
-        	camSes1 = NIVision.IMAQdxOpenCamera("cam1", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        }
-        catch(VisionException e)
-        {
-        	SmartDashboard.putString("Camera error @ camSes1 open", e.toString());
-        }
-        
-        try 
-        {
-        	NIVision.IMAQdxConfigureGrab(camSes1);
-        }
-        catch(VisionException e)
-        {
-        	SmartDashboard.putString("Camera error @ camSes1 configGrab", e.toString());
-        }
-    //state variables
-        firstRun = true;
-        useCam0 = true;
-        toggleCamera = true;
-    }
-    
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser chooser;
+
+	//global variables
+
+	//motors - note: make sure CAN devices are numbered properly in the web interface (connect roborio and go to 172.22.11.2). 
+	//Start device IDs from 1. Leave 0 free for new devices.
+	//code refers to device 1 with an index of 0
+	CANTalon[] motorlist; //stores all motors
+
+	//joysticks - note: buttons start at 1
+	Joystick joyDrive; //driver joystick
+	Joystick joyManip; //manipulator joystick
+
+	//joystick values
+	double leftDrive; //stores left joystick y axis value
+	double rightDrive; //stores right joystick y axis value
+
+
+	//compressor
+	Compressor comp;
+
+	//valves
+
+	//camera
+	int camSes0;
+	int camSes1;
+	Image frame;
+
+	//controller axes
+	final int leftY = 1;
+	final int rightY = 5;
+	final int yButton = 4;
+
+	//cameraServos
+
+	Servo pan;
+	Servo tilt;
+
+	double panVal;
+	double tiltVal;
+
+
+	//state variables
+	boolean firstRun;
+	boolean useCam0;
+	boolean toggleCamera; //to check if in the process of toggle (true = can toggle, false = DO NOT TOGGLE YET)
+	boolean justSwitched;
+
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		chooser = new SendableChooser();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+
+
+
+		//motorlist
+		motorlist = new CANTalon[6];
+		for(int i=0; i<=5; i++)
+		{
+			motorlist[i] = new CANTalon(i+1);
+		}
+
+		//enable brake mode for drive motors
+		for(int i=0; i<=3; i++)
+		{
+			motorlist[i].enableBrakeMode(true);
+		}
+
+		//joysticks
+		joyDrive = new Joystick(0);
+		joyManip = new Joystick(1);
+
+
+		//compressor
+		comp = new Compressor();
+		//enable
+		comp.start();
+
+		//valves
+
+		//camera
+
+		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+		//camSes0
+		try 
+		{
+			camSes0 = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+		}
+		catch(VisionException e)
+		{
+			SmartDashboard.putString("Camera error @ camSes0 open", e.toString());
+		}
+
+		try 
+		{
+			NIVision.IMAQdxConfigureGrab(camSes0);
+		}
+		catch(VisionException e)
+		{
+			SmartDashboard.putString("Camera error @ camSes0 configGrab", e.toString());
+		}
+
+
+
+		//camSes1
+		try 
+		{
+			camSes1 = NIVision.IMAQdxOpenCamera("cam1", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+		}
+		catch(VisionException e)
+		{
+			SmartDashboard.putString("Camera error @ camSes1 open", e.toString());
+		}
+
+		try 
+		{
+			NIVision.IMAQdxConfigureGrab(camSes1);
+		}
+		catch(VisionException e)
+		{
+			SmartDashboard.putString("Camera error @ camSes1 configGrab", e.toString());
+		}
+
+		//cameraServos
+
+		pan = new Servo(0);
+		tilt = new Servo(1);
+
+		panVal = 70;
+		tiltVal = 70;
+
+
+		//state variables
+		firstRun = true;
+		useCam0 = true;
+		toggleCamera = true;
+		justSwitched = true;
+	}
+
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
 	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
@@ -172,34 +187,36 @@ public class Robot extends IterativeRobot {
 	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
-    public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+	@Override
+	public void autonomousInit() {
+		autoSelected = (String) chooser.getSelected();
+		//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
-    }
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-    		
-    		
-    		//logitech controllers, no rumble.
-    		/*
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch(autoSelected) {
+		case customAuto:
+			//Put custom auto code here   
+			break;
+		case defaultAuto:
+		default:
+			//Put default auto code here
+
+
+			//logitech controllers, no rumble.
+			/*
         		for(int i = 0; i<5; i++)
         		{
         			joyDrive.setRumble(RumbleType.kLeftRumble, i%2 + 1);
         			joyDrive.setRumble(RumbleType.kRightRumble, i%2);
         			Timer.delay(.1);
         		}
-        	
+
         		joyDrive.setRumble(RumbleType.kLeftRumble, 0);
         		joyDrive.setRumble(RumbleType.kRightRumble, 0);
         		joyManip.setRumble(RumbleType.kLeftRumble, 1);
@@ -207,124 +224,195 @@ public class Robot extends IterativeRobot {
         		Timer.delay(.5);
         		joyDrive.setRumble(RumbleType.kLeftRumble, 0);
         		joyDrive.setRumble(RumbleType.kRightRumble, 0);
-        	*/
-        	
-            break;
-    	}
-    }
+			 */
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-      	
-    	//starts up cameras on first teleop cycle
-    	if(firstRun)
-    	{
-            
-            //to draw a rect, uncomment and modify the line below; also for one more right after image grab ***
-            //NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+			break;
+		}
+	}
 
-    	}
-    	
-    	//toggle for cameras
-    	
-    	if(joyDrive.getRawButton(yButton) && toggleCamera)
-    	{
-    		toggleCamera = false; //on first press, disable toggle
-    		useCam0 = !useCam0;	//switch cameras, if button is still held down, it will not toggle anymore, and will not re-enable toggle
-    	}
-    	else if(joyDrive.getRawButton(yButton) == false) //when button is finally released, re-enable toggle
-    	{
-    		toggleCamera = true;
-    	}
-      	
-    	//grabs image
-    	
-    	if(useCam0)
-    	{
-    		
-    		try
-    		{
-    			NIVision.IMAQdxStopAcquisition(camSes1);
-    		}
-    		catch(VisionException e)
-    		{
-            	SmartDashboard.putString("Camera error @ camSes1 stopAcq", e.toString());
-    		}
-    		
-    		
-    		try
-    		{
-    			NIVision.IMAQdxStartAcquisition(camSes0);
-    		}
-    		catch(VisionException e)
-    		{
-            	SmartDashboard.putString("Camera error @ camSes0 startAcq", e.toString());
-    		}
-    		
-    	}
-    	else
-    	{
-    		
-    		try
-    		{
-    			NIVision.IMAQdxStopAcquisition(camSes0);
-    		}
-    		catch(VisionException e)
-    		{
-            	SmartDashboard.putString("Camera error @ camSes0 stopAcq", e.toString());
-    		}
-    		
-    		
-    		try
-    		{
-    			NIVision.IMAQdxStartAcquisition(camSes1);
-    		}
-    		catch(VisionException e)
-    		{
-            	SmartDashboard.putString("Camera error @ camSes1 startAcq", e.toString());
-    		}
-    	}
-    	
-    	
-    	try
-    	{
-    		NIVision.IMAQdxGrab(useCam0 ? camSes0 : camSes1, frame, 1);
-    	}
-    	catch(VisionException e)
-    	{
-        	SmartDashboard.putString("Camera error @ grabImage", e.toString());
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
 
-    	}
-    	
+		//starts up cameras on first teleop cycle
+		if(firstRun)
+		{
+
+			//to draw a rect, uncomment and modify the line below; also for one more right after image grab ***
+			//NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+
+		}
+
+		//toggle for cameras
+
+		if(joyDrive.getRawButton(yButton) && toggleCamera)
+		{
+			toggleCamera = false; //on first press, disable toggle
+			useCam0 = !useCam0;	//switch cameras, if button is still held down, it will not toggle anymore, and will not re-enable toggle
+
+			justSwitched = true;
+		}
+		else if(!joyDrive.getRawButton(yButton)) //when button is finally released, re-enable toggle
+		{
+			toggleCamera = true;
+		}
+
+		//grabs image
+
+		if(useCam0 && justSwitched)
+		{
+
+			try
+			{
+				NIVision.IMAQdxStopAcquisition(camSes1);
+			}
+			catch(VisionException e)
+			{
+				SmartDashboard.putString("Camera error @ camSes1 stopAcq", e.toString());
+			}
+
+
+			try
+			{
+				NIVision.IMAQdxStartAcquisition(camSes0);
+			}
+			catch(VisionException e)
+			{
+				SmartDashboard.putString("Camera error @ camSes0 startAcq", e.toString());
+			}
+
+			justSwitched = false;
+
+		}
+		else if(!useCam0 && justSwitched)
+		{
+
+			try
+			{
+				NIVision.IMAQdxStopAcquisition(camSes0);
+			}
+			catch(VisionException e)
+			{
+				SmartDashboard.putString("Camera error @ camSes0 stopAcq", e.toString());
+			}
+
+
+			try
+			{
+				NIVision.IMAQdxStartAcquisition(camSes1);
+			}
+			catch(VisionException e)
+			{
+				SmartDashboard.putString("Camera error @ camSes1 startAcq", e.toString());
+			}
+
+			justSwitched = false;
+		}
+
+
+		try
+		{
+			NIVision.IMAQdxGrab(useCam0 ? camSes0 : camSes1, frame, 1);
+		}
+		catch(VisionException e)
+		{
+			SmartDashboard.putString("Camera error @ grabImage", e.toString());
+
+		}
+
 		//***uncomment to draw predefined rectangle
 		// NIVision.imaqDrawShapeOnImage(frame, frame, rect,DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-		
-		
+
+
 		//sends to camera
 		CameraServer.getInstance().setImage(frame);
-    	
+
 		//joystick code
-    	leftDrive = joyDrive.getRawAxis(leftY); 
-        rightDrive = joyDrive.getRawAxis(rightY);
-        
-        motorlist[0].set(-1 * leftDrive);
-        motorlist[1].set(-1 * leftDrive);
-        motorlist[2].set(-1 * rightDrive);
-        motorlist[3].set(-1 * rightDrive);
-        
-        
-        
-        firstRun = false;
-        
-       
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-  	
-    }   
+		leftDrive = joyDrive.getRawAxis(leftY); 
+		rightDrive = joyDrive.getRawAxis(rightY);
+
+
+		//motor code
+
+		motorlist[0].set(-1 * leftDrive);
+		motorlist[1].set(-1 * leftDrive);
+		motorlist[4].set(1 * rightDrive);
+		motorlist[5].set(1 * rightDrive);
+
+
+		//servo code
+
+		//default position
+
+		if(joyDrive.getRawButton(7)==true)
+		{
+			panVal=70;
+			tiltVal=70;
+			pan.setAngle(panVal);
+			tilt.setAngle(tiltVal);
+		}
+
+		//PAN rotating left and right
+
+		if(joyDrive.getPOV(0)==90 && panVal>=0 && panVal<=180)
+		{
+			panVal=panVal-5;
+			if(panVal<0)
+			{
+				panVal = 0;
+			}
+			pan.setAngle(panVal);
+		}
+
+		if(joyDrive.getPOV(0)==270 && panVal>=0 && panVal<=180)
+		{
+			panVal=panVal+5;
+			if(panVal>180)
+			{
+				panVal = 180;
+			}
+			pan.setAngle(panVal);
+		}
+
+
+
+		//TILT rotating up and down
+
+		if(joyDrive.getPOV(0)== 0 && tiltVal >=0 && tiltVal <=180)
+		{
+			tiltVal = tiltVal-2;
+			if(tiltVal < 0)
+			{
+				tiltVal = 0;
+			}
+			tilt.setAngle(tiltVal);
+		}
+
+		if(joyDrive.getPOV(0)== 180 && tiltVal >=0 && tiltVal <=180)
+		{
+			tiltVal = tiltVal+2;
+			if(tiltVal > 180)
+			{
+				tiltVal = 180;
+			}
+			tilt.setAngle(tiltVal);
+		}
+
+
+
+		//sets to false on first run
+		firstRun = false;
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+
+	}   
 }
+
